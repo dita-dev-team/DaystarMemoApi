9cc4612c79fbca7e36774020bfa1e2a6851ff693
md-arin <graffymint@gmail.com>
2017-03-07 15:20:31 +0300

    Finished store memo to database: Yet to add files and store url to database

--- app/Http/Controllers/MemoController.php
+++ app/Http/Controllers/MemoController.php
@@ -16,10 +16,11 @@ class MemoController extends Controller
      */
     public function index()
     {
+
         $memos = Memo::all();
         $result = array();
 
-        foreach ($memos as $memo){
+        foreach ($memos as $memo) {
             array_push($result, [
                 'id' => $memo->id,
                 'file_url' => $memo->file_url,
@@ -29,7 +30,7 @@ class MemoController extends Controller
                 'time' => $memo->created_at
             ]);
         }
-        return  response()->json($result);
+        return response()->json($result, 200);
     }
 
     /**
@@ -45,19 +46,58 @@ class MemoController extends Controller
     /**
      * Store a newly created resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
+     * @param  \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
     public function store(Request $request)
     {
-        $user = $request->user('id');
+
+        $memo = new Memo;
+        $user = new User;
+
+        if ($request->get('user_id') == null || $request->get('body_content') == null || $request->get('to') == null) {
+            return response()->json('parameters missing', 403);
+        } elseif (!ctype_digit(strval($request->get('user_id'))) || !ctype_digit(strval($request->get('to')))) {
+            return response()->json(['Invalid User Id Type', 'or', 'Invalid Recipient Id', 'Expecting Int Value'], 403);
+        } else {
+
+            if (!$user->find($request->get('user_id'))) {
+                return response()->json('User Id Not Found', 400);
+            } elseif (!$user->find($request->get('to'))) {
+                return response()->json('Recipient User Id Not Found', 400);
+            }
+
+            $user_id = $user->find($request->get('user_id'))->id;
+            $content = $request->body_content;
+            $to = $user->find($request->get('to'))->id;
+
+//         TODO get files and save them. Get uri and save in database
+            if ($request->hasFile('file_url')) {
+                $file = $request->file('file_url');
+
+                if ($file->getType() /* get file type image*/) {
+                    /*Save the image file to disk and get url*/
+                } elseif ($file->getType() /*get other file type */) {
+                    /*Save the file to disk and get url*/
+                }
+            }
+
+
+            $memo->user_id = $user_id;
+            $memo->content = $content;
+            $memo->to = $to;
+//         TODO add file_url to database whenever there is a file included in the memo
+
+            $memo->save();
+            return response()->json('Saved', 200);
+        }
 
     }
 
     /**
      * Display the specified resource.
      *
-     * @param  \App\Memo  $memo
+     * @param  \App\Memo $memo
      * @return \Illuminate\Http\Response
      */
     public function show(Memo $memo)
@@ -68,7 +108,7 @@ class MemoController extends Controller
     /**
      * Show the form for editing the specified resource.
      *
-     * @param  \App\Memo  $memo
+     * @param  \App\Memo $memo
      * @return \Illuminate\Http\Response
      */
     public function edit(Memo $memo)
@@ -79,8 +119,8 @@ class MemoController extends Controller
     /**
      * Update the specified resource in storage.
      *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Memo  $memo
+     * @param  \Illuminate\Http\Request $request
+     * @param  \App\Memo $memo
      * @return \Illuminate\Http\Response
      */
     public function update(Request $request, Memo $memo)
@@ -91,7 +131,7 @@ class MemoController extends Controller
     /**
      * Remove the specified resource from storage.
      *
-     * @param  \App\Memo  $memo
+     * @param  \App\Memo $memo
      * @return \Illuminate\Http\Response
      */
     public function destroy(Memo $memo)
--- app/Memo.php
+++ app/Memo.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Memo extends Model
 {
-    protected $fillable = ['content','file_url'];
+    protected $fillable = ['user_id', 'content', 'to', 'file_url'];
 
     public function user()
     {
