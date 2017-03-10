75b63aadeca059137ecb19e9872d1eceef18c92b
md-arin <graffymint@gmail.com>
2017-03-10 21:18:46 +0300

    Included image upload test in memos

--- tests/MemoTest.php
+++ tests/MemoTest.php
@@ -6,6 +6,8 @@
  * Time: 11:14 AM
  */
 
+use \Illuminate\Http\UploadedFile;
+
 class MemoTest extends TestCase
 {
     /*
@@ -20,6 +22,8 @@ class MemoTest extends TestCase
     {
         //Testing for proper parameters and saving
         $this->json('POST', '/api/memos', ['user_id' => 1, 'memo_body' => 'Memo Test Assertion', 'to' => 1])->assertResponseStatus(200);
+        $file = new UploadedFile('/home/arin/Google Drive/Pictures/Awesome/16406539_714826678681053_482536755507822811_n.jpg','16406539_714826678681053_482536755507822811_n.jpg', filesize('/home/arin/Google Drive/Pictures/Awesome/'), 'image/jpg', null, true);
+        $this->json('POST', '/api/memos', ['user_id' => 1, 'memo_body' => 'Working on Coverage', 'to' => 1, 'file' => $file])->assertResponseStatus(200);
 
         // Testing for wrong input or too few parameters
         $this->json('POST', 'api/memos')->assertResponseStatus(403);
