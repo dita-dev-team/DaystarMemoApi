7fea1319229a972bf8ba05fa6a407c0c4257224c
md-arin <graffymint@gmail.com>
2017-04-07 18:51:44 +0300

    updated tests on updated memos to groups configuration

--- tests/MemoTest.php
+++ tests/MemoTest.php
@@ -22,23 +22,43 @@ class MemoTest extends TestCase
     {
         //Testing for proper parameters and saving
         $users = factory(App\User::class, 10)->create();
+        $group = factory(App\Group::class, 10)->create();
 
-        $this->json('POST', '/api/memos', ['user_id' => $users->first()->id, 'memo_body' => 'Memo Test Assertion', 'to' => $users->first()->id])->assertResponseStatus(200);
+//        User Tests
+        $this->json('POST', '/api/memos', ['user_id' => $users->first()->id, 'memo_body' => 'Memo Test Assertion', 'to_user' => $users->first()->id])->assertResponseStatus(200);
         /*
-         * Testing saving including the file uploads.
+         * Testing User saving including the file uploads.
          * Remember to copy another image in the directory storage/testing since the MemoController moves file to intended location
          * Rename the image image.jpg
          */
         $file = new UploadedFile(storage_path('testing/image.jpg'),'image.jpg', filesize(storage_path('testing/image.jpg')), 'image/jpg', null, true);
-        $this->json('POST', '/api/memos', ['user_id' => $users->first()->id, 'memo_body' => 'Working on Coverage', 'to' => $users->first()->id, 'file' => $file])->assertResponseStatus(200);
+        $this->json('POST', '/api/memos', ['user_id' => $users->first()->id, 'memo_body' => 'Working on Coverage', 'to_user' => $users->first()->id, 'file' => $file])->assertResponseStatus(200);
 
         // Testing for wrong input or too few parameters
         $this->json('POST', 'api/memos')->assertResponseStatus(403);
         $this->json('POST', 'api/memos', ['user_id' => random_int(1,9)])->assertResponseStatus(403);
         $this->json('POST', 'api/memos', ['user_id' => random_int(1,9), 'memo_body' => 'Crazy Tests'])->assertResponseStatus(403);
-        $this->json('POST', 'api/memos', ['user_id' => 'e', 'memo_body' => 'Tests huh!', 'to' => 'e'])->assertResponseStatus(403);
-        $this->json('POST', 'api/memos', ['user_id' => random_int(11,20), 'memo_body' => 'Tests Again!', 'to' => random_int(11,20)])->assertResponseStatus(400);
-        $this->json('POST', 'api/memos', ['user_id' => random_int(11, 20), 'memo_body' => 'Tests Lord Have Mercy!', 'to' => random_int(11,20)])->assertResponseStatus(400);
+        $this->json('POST', 'api/memos', ['user_id' => 'e', 'memo_body' => 'Tests huh!', 'to_user' => 'e'])->assertResponseStatus(403);
+        $this->json('POST', 'api/memos', ['user_id' => random_int(11,20), 'memo_body' => 'Tests Again!', 'to_user' => random_int(20000,21000)])->assertResponseStatus(404);
+        $this->json('POST', 'api/memos', ['user_id' => random_int(11, 20), 'memo_body' => 'Tests Lord Have Mercy!', 'to_user' => random_int(20000,21000)])->assertResponseStatus(404);
+
+//        Group Tests
+        $this->json('POST', '/api/memos', ['user_id' => $users->first()->id, 'memo_body' => 'Memo Test Assertion', 'to_group' => $group->first()->id])->assertResponseStatus(200);
+        /*
+         * Testing User saving including the file uploads.
+         * Remember to copy another image in the directory storage/testing since the MemoController moves file to intended location
+         * Rename the image image.jpg
+         */
+        $file = new UploadedFile(storage_path('testing/image_1.jpg'),'image.jpg', filesize(storage_path('testing/image_1.jpg')), 'image/jpg', null, true);
+        $this->json('POST', '/api/memos', ['user_id' => $users->first()->id, 'memo_body' => 'Working on Coverage', 'to_group' => $group->first()->id, 'file' => $file])->assertResponseStatus(200);
+
+        // Testing for wrong input or too few parameters
+        $this->json('POST', 'api/memos')->assertResponseStatus(403);
+        $this->json('POST', 'api/memos', ['user_id' => random_int(1,9)])->assertResponseStatus(403);
+        $this->json('POST', 'api/memos', ['user_id' => random_int(1,9), 'memo_body' => 'Crazy Tests'])->assertResponseStatus(403);
+        $this->json('POST', 'api/memos', ['user_id' => 'e', 'memo_body' => 'Tests huh!', 'to_group' => 'e'])->assertResponseStatus(403);
+        $this->json('POST', 'api/memos', ['user_id' => random_int(11,20), 'memo_body' => 'Tests Again!', 'to_group' => random_int(20000,21000)])->assertResponseStatus(404);
+        $this->json('POST', 'api/memos', ['user_id' => random_int(11, 20), 'memo_body' => 'Tests Lord Have Mercy!', 'to_group' => random_int(20000,21000)])->assertResponseStatus(404);
 
     }
 }
